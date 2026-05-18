08638161b6df5a198fd8f7a6ae2eac284e31f827
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-05-18 17:20:18 +0100

    Implement creola::expand::Expander::expand() method

--- src/expand.rs
+++ src/expand.rs
@@ -3,7 +3,7 @@ use crate::dsl::Expr;
 pub struct Expander;
 
 impl Expander{
-    pub fn expand(expr: &Expr) -> Expr {
-        todo!("")
+    pub fn expand(expr: &Expr) -> Result<Expr, String> {
+        Ok(expr.clone())
     }
 }
