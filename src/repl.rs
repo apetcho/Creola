c7edf204e810e011b6f015396339b6a66d827abb
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-05-18 20:00:34 +0100

    Implement creola::repl::Repl::handle_expand() method

--- src/repl.rs
+++ src/repl.rs
@@ -392,7 +392,40 @@ impl Repl{
     }
 
     fn handle_expand(&mut self, input: &str) -> Result<Option<String>, String> {
-        todo!("")
+        // expand(expr)
+        let first = match input.find('('){
+            Some(p) => p,
+            None => {
+                return Ok(None);
+            }
+        };
+
+        let last = match input.rfind(')'){
+            Some(p) => p,
+            None => {
+                return Ok(None);
+            }
+        };
+        let inner = &input[first+1..last];
+        let mut parser = match Parser::new(input){
+            Ok(p) => p,
+            Err(msg) => {
+                return Err(msg);
+            }
+        };
+        let expr = match parser.parse_expr(){
+            Ok(e) => e,
+            Err(msg) => {
+                return Err(msg);
+            }
+        };
+        let ans = match Creola::expand(&expr){
+            Ok(e) => e,
+            Err(msg) => {
+                return Err(msg);
+            }
+        };
+        Ok(Some(format!("{}", ans)))
     }
 
     fn handle_simplify(&mut self, input: &str) -> Result<Option<String>, String> {
