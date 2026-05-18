5b6d8cb18a0432fe7a21708aa9153cb5e69c1f53
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-05-18 14:25:56 +0100

    Implement creola::diff::Differentiator::diff() method

--- src/diff.rs
+++ src/diff.rs
@@ -3,19 +3,32 @@ use crate::dsl::{Expr, UnaryOp, BinaryOp};
 pub struct Differentiator;
 
 impl Differentiator{
-    pub fn diff(expr: &Expr, var: &str) -> Expr {
+    pub fn diff(expr: &Expr, var: &str) -> Result<Expr, String> {
+        match expr{
+            Expr::Num(_) => Ok(Expr::Num(0.0)),
+            Expr::Var(name) => {
+                if name == var { Ok(Expr::Num(1.0)) }
+                else{ Ok(Expr::Num(0.0)) }
+            }
+            Expr::Unary(_, rhs) => Differentiator::diff_unary(rhs, var),
+            Expr::Binary(op, lhs, rhs) => {
+                Differentiator::diff_binary(op.clone(), lhs, rhs, var)
+            }
+            Expr::Call(name, args) => {
+                Differentiator::diff_function_call(name, args.clone())
+            }
+        }
+    }
+
+    fn diff_unary(expr: &Expr, var: &str) -> Result<Expr, String> {
         todo!("")
     }
 
-    fn diff_unary(expr: &Expr, var: &str) -> Expr {
+    fn diff_binary(op: BinaryOp, lhs: &Expr, rhs: &Expr, var: &str) -> Result<Expr, String> {
         todo!("")
     }
 
-    fn diff_binary(op: BinaryOp, lhs: &Expr, rhs: &Expr, var: &str) -> Expr {
-        todo!("")
-    }
-
-    fn diff_function_call(name: &str, args: Vec<Expr>) -> Expr {
+    fn diff_function_call(name: &str, args: Vec<Expr>) -> Result<Expr, String> {
         todo!("")
     }
 }
