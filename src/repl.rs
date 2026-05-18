c642dd82f65c388c080853b1a120f9a0b2fd7092
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-05-18 20:05:46 +0100

    Implement creola::repl::Repl::handle_simplify() method

--- src/repl.rs
+++ src/repl.rs
@@ -429,6 +429,41 @@ impl Repl{
     }
 
     fn handle_simplify(&mut self, input: &str) -> Result<Option<String>, String> {
-        todo!("")
+        // simplify(expr)
+        let first = match input.find('('){
+            Some(p) => p,
+            None => {
+                return Ok(None);
+            }
+        };
+
+        let last = match input.rfind(')'){
+            Some(p) => p,
+            None => {
+                return Ok(None);
+            }
+        };
+
+        let inner = &input[first+1..last];
+        let mut parser = match Parser::new(input){
+            Ok(p) => p,
+            Err(msg) => {
+                return Err(msg);
+            }
+        };
+        let expr = match parser.parse_expr(){
+            Ok(e) => e,
+            Err(msg) => {
+                return Err(msg);
+            }
+        };
+        let ans = match Creola::simplify(&expr){
+            Ok(e) => e,
+            Err(msg) => {
+                return Err(msg);
+            }
+        };
+
+        Ok(Some(format!("{}", ans)))
     }
 }
