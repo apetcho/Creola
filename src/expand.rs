fbe11f442783f1c2dd8df79481c5f08f0b66af8a
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-05-19 14:18:47 +0100

    Refactor creola::expand::Expander::expand_help() method

--- src/expand.rs
+++ src/expand.rs
@@ -1,9 +1,57 @@
-use crate::dsl::Expr;
+use crate::dsl::{Expr, BinaryOp, UnaryOp};
 
 pub struct Expander;
 
 impl Expander{
     pub fn expand(expr: &Expr) -> Result<Expr, String> {
-        Ok(expr.clone())
+        Expander::expand_helper(expr)
+    }
+
+    fn expand_helper(expr: &Expr) -> Result<Expr, String> {
+        match expr {
+            Expr::Num(_) | Expr::Var(_) => Ok(expr.clone()),
+
+            Expr::Unary(op, rhs) => {
+                let e = Expander::expand_helper(rhs)?;
+                Ok(Expr::Unary(*op, Box::new(e)))
+            }
+
+            Expr::Binary(op, lhs, rhs) => {
+                let lhs = Expander::expand_helper(lhs)?;
+                let rhs = Expander::expand_helper(rhs)?;
+                match op {
+                    BinaryOp::Add => Ok(Expr::Binary(BinaryOp::Add, Box::new(lhs), Box::new(rhs))),
+                    BinaryOp::Sub => Ok(Expr::Binary(BinaryOp::Sub, Box::new(lhs), Box::new(rhs))),
+                    BinaryOp::Mul => Expander::expand_mul(lhs, rhs),
+                    BinaryOp::Div => {
+                        // we don't distribute division; just expand numberator/denominator
+                        Ok(Expr::Binary(BinaryOp::Div, Box::new(lhs), Box::new(rhs)))
+                    }
+                    BinaryOp::Pow => Expander::expand_pow(lhs, rhs),
+                }
+            }
+
+            Expr::Call(name, args) => {
+                let mut argv = vec![];
+                for arg in args.iter() {
+                    let arg = Expander::expand_helper(arg)?;
+                    argv.push(arg);
+                }
+
+                Ok(Expr::Call(name.clone(), argv))
+            }
+        }
+    }
+
+    fn expand_mul(lhs: Expr, rhs: Expr) -> Result<Expr, String> {
+        todo!("")
+    }
+
+    fn expand_pow(base: Expr, expo: Expr) -> Result<Expr, String> {
+        todo!("")
+    }
+
+    fn expand_pow_int(base: Expr, n: usize) -> Result<Expr, String> {
+        todo!("")
     }
 }
