fbd63992f3bf893d960246e94277cea0b2fb7de1
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-07-06 14:35:55 +0100

    Implement creola::core::Symbol::simplify() method

--- creola-core/core-expr.cpp
+++ creola-core/core-expr.cpp
@@ -5,7 +5,9 @@
 // -*----------------------------------------------------------------*-
 namespace creola{
 //
-
+// -------------------------
+// --- Number Expression ---
+// -------------------------
 Expr Number::simplify(void) const{
     return std::make_shared<Number>(*this);
 }
@@ -69,13 +71,73 @@ void Number::print_unicode(std::ostream& os, int prec) const{
     return;
 }
 
+// -------------------------
+// --- Symbol Expression ---
+// -------------------------
+Expr Symbol::simplify(void) const{
+    return std::make_shared<Symbol>(*this);
+}
+
+//
+Expr Symbol::diff(const std::string& var) const{
+    //! @todo
+    return nullptr;
+}
+Expr Symbol::integrate(const std::string& var) const{
+    //! @todo
+    return nullptr;
+}
+Expr Symbol::expand(void) const{
+    //! @todo
+    return nullptr;
+}
+Expr Symbol::factor(const std::string& var) const{
+    //! @todo
+    return nullptr;
+}
+Expr Symbol::groebner(const Vec<Expr>& exprs) const{
+    //! @todo
+    return nullptr;
+}
+Expr Symbol::taylor(const std::string& var, f64 val, int n) const{
+    //! @todo
+    return nullptr;
+}
+Vec<f64> Symbol::roots(const std::string& var, f64 vmin, f64 vmax, int samples) const{
+    //! @todo
+    return {};
+}
+f64 Symbol::limit(const std::string& var, f64 val, f64 eps) const{
+    //! @todo
+    return 0.0;
+}
+
+f64 Symbol::eval(const std::string& var, f64 val) const{
+    //! @todo
+    return 0.0;
+}
+
+Box Symbol::to_box(void) const{
+    //! @todo
+    return Box();
+}
+
+Box Symbol::to_box_prec(int parent_prec) const{
+    //! @todo
+    return Box();
+}
+void Symbol::print_unicode(std::ostream& os, int prec) const{
+    //! @todo
+    return;
+}
+
+
 /*
 
-// -*-
-struct Number : public ExprBase{
-    f64 value;
-    explicit Number(f64 val)
-    : ExprBase{ExprKind::Number}, value{val}{}
+struct Symbol : public ExprBase{
+    std::string name;
+    explicit Symbol(const std::string& name)
+    : ExprBase{ExprKind::Symbol}, name{std::move(name)} {}
 
 
 };
