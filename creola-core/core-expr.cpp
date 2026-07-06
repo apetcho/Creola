1c2613631f24c93c3b720cf818abc7bf472e2bd7
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-07-06 12:26:37 +0100

    Implement creola::core::Number::diff() method

--- creola-core/core-expr.cpp
+++ creola-core/core-expr.cpp
@@ -10,6 +10,58 @@ Expr Number::simplify(void) const{
     return std::make_shared<Number>(*this);
 }
 
+// -*-
+Expr Number::diff(const std::string& var) const{
+    CREOLA_UNUSED(var);
+    return std::make_shared<Number>(0.0);
+}
+
+Expr Number::integrate(const std::string& var) const{
+    //! @todo
+    return nullptr;
+}
+Expr Number::expand(void) const{
+    //! @todo
+    return nullptr;
+}
+Expr Number::factor(const std::string& var) const{
+    //! @todo
+    return nullptr;
+}
+Expr Number::groebner(const Vec<Expr>& exprs) const{
+    //! @todo
+    return nullptr;
+}
+Expr Number::taylor(const std::string& var, f64 val, int n) const{
+    //! @todo
+    return nullptr;
+}
+Vec<f64> Number::roots(const std::string& var, f64 vmin, f64 vmax, int samples) const{
+    //! @todo
+    return {};
+}
+f64 Number::limit(const std::string& var, f64 val, f64 eps=1e-6) const{
+    //! @todo
+    return 0.0;
+}
+
+f64 Number::eval(const std::string& var, f64 val) const{
+    //! @todo
+    return 0.0;
+}
+
+Box Number::to_box(void) const{
+    //! @todo
+    return Box();
+}
+Box Number::to_box_prec(int parent_prec) const{
+    //! @todo
+    return Box();
+}
+void Number::print_unicode(std::ostream& os, int prec) const{
+    //! @todo
+    return;
+}
 
 /*
 
@@ -19,21 +71,7 @@ struct Number : public ExprBase{
     explicit Number(f64 val)
     : ExprBase{ExprKind::Number}, value{val}{}
 
-Expr Number::simplify(void) const override;
-Expr Number::diff(const std::string& var) const override;
-Expr Number::integrate(const std::string& var) const override;
-Expr Number::expand(void) const override;
-Expr Number::factor(const std::string& var) const override;
-Expr Number::groebner(const Vec<Expr>& exprs) const override;
-Expr Number::taylor(const std::string& var, f64 val, int n) const override;
-Vec<f64> Number::roots(const std::string& var, f64 vmin=-10, f64 vmax=10, int samples=100) const override;
-f64 Number::limit(const std::string& var, f64 val, f64 eps=1e-6) const override;
 
-f64 eval(const std::string& var, f64 val) const override;
-
-Box Number::to_box(void) const override;
-Box Number::to_box_prec(int parent_prec) const override;
-void Number::print_unicode(std::ostream& os, int prec=0) const override;
 };
 
 */
