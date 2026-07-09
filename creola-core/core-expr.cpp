0be1e71d411450ecf4c83ee25f83943da5c61a8c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-07-09 15:03:48 +0100

    Implement creola::core::::eval() method

--- creola-core/core-expr.cpp
+++ creola-core/core-expr.cpp
@@ -152,7 +152,9 @@ f64 Neg::limit(const std::string& var, f64 val, f64 eps=1e-6) const{
 }
 
 f64 Neg::eval(const std::string& var, f64 val) const{
-    //! @todo
+    if(auto sym = std::dynamic_pointer_cast<Symbol>(this->arg)){
+        if(sym->name==var){ return -1.0 * val; }
+    }
     return 0.0;
 }
 
