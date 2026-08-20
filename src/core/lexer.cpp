8d1733fdf6ff6dab84666b3eb8907730a5bc812e
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-08-20 10:47:25 +0100

    Implement creola::core::Lexer::next() method

--- src/core/lexer.cpp
+++ src/core/lexer.cpp
@@ -6,8 +6,42 @@
 namespace creola::core{
 //
 
+// -*-
 Lexer::Lexer(const Str& src) : m_src{}
 {}
+
+// -*-
+Token Lexer::next(void){
+    this->skip_ws();
+    if(this->is_eos()){ return Token(); }
+
+    auto c = this->peek();
+    // read number 
+    if(std::isdigit(c) || c == '.'){ return this->read_number(); }
+
+    // read string := "text" | 'text'
+    if(c=='"' || c == '\''){ return this->read_string(); }
+
+    // read identifier
+    if(std::isalpha(c) || c == '_'){ return this->read_identifier(); }
+
+    // -*-
+    this->advance();
+    switch(c){
+        case '+': return Token(TokenKind::Plot, "+");
+        case '-': return Token(TokenKind::Minus, "-");
+        case '*': return Token(TokenKind::Star, "*");
+        case '/': return Token(TokenKind::Slash, "/");
+        case '^': return Token(TokenKind::Caret, "^");
+        case '(': return Token(TokenKind::LParen, "(");
+        case ')': return Token(TokenKind::RParen, ")");
+        case ',': return Token(TokenKind::Comma, ",");
+        case '=': return Token(TokenKind::Equal, "=");
+        case ';': return Token(TokenKind::Semi, ";");
+    }
+
+    return Token();
+}
 
 /*
 // -*-
