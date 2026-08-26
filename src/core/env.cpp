2d1d4a25673fab3c036d767815fffea439f209ff
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-08-26 12:50:14 +0100

    Implement creola::core::Env::assign() method

--- src/core/env.cpp
+++ src/core/env.cpp
@@ -16,12 +16,24 @@ void Env::define(const Str& name, const Expr& expr){
     this->m_bindings[name] = expr;
 }
 
+// -*-
+Expr Env::assign(const Str& name, const Expr& expr){
+    auto entry = this->m_bindings.find(name);
+    if(entry == this->m_bindings.end() && this->m_parent==nullptr){
+        throw CreolaError("undefined +'" + name + "'");
+    }
+    if(entry != this->m_bindings.end()){
+        return entry->second;
+    }
+
+    return this->m_parent->assign(name, expr);
+}
+
 /*
 class Env final: std::enable_shared_from_this<Env>{
 public:
 
 
-Expr Env::assign(const Str& name, const Expr& expr);
 Expr& Env::get(const Str& name) const;
 void Env::pprint(PrettyPrinter& pprinter) const;
 
