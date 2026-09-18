702ea70a1c5e4c2daba77dbc65cf85cb1c764b43
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-18 19:23:06 +0100

    Define Parser class

--- src/core/parser.hpp
+++ src/core/parser.hpp
@@ -1,11 +1,42 @@
 #pragma once
 
+#include "ast.hpp"
+#include "tokenizer.hpp"
+#include<memory>
+#include<vector>
 
 // -*----------------------------------------------------------------*-
 // -*- begin::namespace::creola                                     -*-
 // -*----------------------------------------------------------------*-
 namespace creola{
 // -
+
+class Parser final {
+public:
+    Parser(const std::string& src);
+    Stmt parse(void);
+
+private:
+    std::vector<Token> m_tokens;
+    std::size_t m_cur;
+
+    Token current(void) const;
+    bool match(TokenKind kind) const;
+    Token consume(TokenKind kind, const std::string& msg);
+
+    Stmt parseStatement(void);
+    Stmt parseLet(void);
+    Stmt parseFun(void);
+
+    Expr parseExpression(void);
+    Expr parseAddSub(void);
+    Expr parseMulDiv(void);
+    Expr parsePow(void);
+    Expr parseUnary(void);
+    Expr parsePrimary(void);
+    Expr parseCall(void);
+    Expr parseLambda(void);
+};
 
 
 // -*----------------------------------------------------------------*-
