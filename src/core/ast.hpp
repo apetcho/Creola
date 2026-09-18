050594a50cdab392a542be4341f14ae32c88453f
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-18 18:46:58 +0100

    Define abstract AST base.

---
+++ src/core/ast.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include<memory>
+#include<string>
+#include<vector>
+#include<variant>
+#include<complex>
+#include<iostream>
+
+
+// -*----------------------------------------------------------------*-
+// -*- begin::namespace::creola                                     -*-
+// -*----------------------------------------------------------------*-
+namespace creola{
+// -
+
+// Forward declarations
+struct ExprBase;
+struct StmtBase;
+struct Env;
+struct EvalVisitor;
+struct ExecuteVisitor;
+
+using Expr = std::shared_ptr<ExprBase>;
+using Stmt = std::shared_ptr<StmtBase>;
+using Value = std::variant<std::int64_t, double, std::complex<double>>;
+
+
+// --- Abstract AST Base ---
+struct ExprBase {
+    virtual ~ExprBase() = default;
+    virtual Expr eval(EvalVisitor&, Env&) const = 0;
+};
+
+struct StmtBase {
+    virtual ~StmtBase() = default;
+    virtual void execute(ExecuteVisitor&, Env&) const = 0;
+};
+
+
+
+// -*----------------------------------------------------------------*-
+}//-*- end::namespace::creola                                       -*-
+// -*----------------------------------------------------------------*-
