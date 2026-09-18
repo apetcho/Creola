a462a370bc3ee54db4c136ca96e5cb285f10ecef
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-18 19:39:22 +0100

    Define the Repl class

--- src/core/repl.hpp
+++ src/core/repl.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#include "parser.hpp"
+#include "evaluator.hpp"
+#include "env.hpp"
+
+#include<string>
+
 
 // -*----------------------------------------------------------------*-
 // -*- begin::namespace::creola                                     -*-
@@ -7,6 +13,17 @@
 namespace creola{
 // -
 
+class Repl final{
+public:
+    Repl();
+    void run(void);
+
+private:
+    Env m_env;
+
+    std::string prompt(void) const;
+    void processLine(const std::string& line);
+};
 
 // -*----------------------------------------------------------------*-
 }//-*- end::namespace::creola                                       -*-
