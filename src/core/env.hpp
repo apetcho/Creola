25de21ab5141e19a925c08669755b3dea58f7e6c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-18 19:08:04 +0100

    Define Env class

--- src/core/env.hpp
+++ src/core/env.hpp
@@ -1,10 +1,29 @@
 #pragma once
 
+#include "ast.hpp"
+#include<map>
+#include<string>
+
+
 // -*----------------------------------------------------------------*-
 // -*- begin::namespace::creola                                     -*-
 // -*----------------------------------------------------------------*-
 namespace creola{
 // -
+
+class Env final {
+public:
+    Env();
+    Env(Env* parent);
+
+    void put(const std::string& key, Expr expr);
+    Expr get(const std::string& key) const;
+    bool hashKey(const std::string& key) const;
+    Env* parent;
+
+private:
+    std::map<std::string, Expr> m_bindings;
+};
 
 
 // -*----------------------------------------------------------------*-
