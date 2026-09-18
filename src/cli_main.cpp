2f7b939e7805cee48e37dc1a0852e2bfa10e26d0
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-18 19:44:14 +0100

    Implement a basic main entry point

--- src/cli_main.cpp
+++ src/cli_main.cpp
@@ -1,3 +1,19 @@
+#include "core/repl.hpp"
+
+#include<iostream>
+
+
+// ------------------------------
+// -*- M A I N    D R I V E R -*-
+// ------------------------------
+int main(void){
+    creola::Repl repl;
+    std::cerr << "Creola Computer Algebra System v0.1" << std::endl;
+    std::cerr << "Type 'exit' to quit." << std::endl;
+    repl.run();
+
+    return 0;
+}
 
 
 /*
