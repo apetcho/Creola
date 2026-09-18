14e4fddbf55c0031a251a2fb62b832f21a4d9bfb
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-18 19:59:56 +0100

    Add test for basic symbolic arithmetic expression evaluation

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -12,7 +12,21 @@ TEST(ParserTest, BasicArithmetic){
 }
 
 
-// TEST(EvaluatorTest, BasicArithmetic){}
+TEST(EvaluatorTest, BasicArithmetic){
+    Parser parser("let x = 2 + 3");
+    auto stmt = parser.parse();
+    Env env;
+    Interpreter interp;
+    stmt->execute(interp, env);
+
+    Expr expr = env.get("x");
+    auto val = expr->eval(interp, env);
+    auto ans = std::dynamic_pointer_cast<Number>(val);
+    EXPECT_NE(ans, nullptr);
+    Value num = ans->value;
+    EXPECT_DOUBLE_EQ(std::get<double>(num), 5.0);
+}
+
 
 // TEST(ParserTest, FunctionDefinition){}
 
