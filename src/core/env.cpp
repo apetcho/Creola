b08919361a1031612976ce53f8aba114b094cc52
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-19 14:58:54 +0100

    Implement creola::Env::get() method

--- src/core/env.cpp
+++ src/core/env.cpp
@@ -20,13 +20,22 @@ void Env::put(const std::string& key, Expr expr){
     this->m_bindings[key] = expr;
 }
 
+// -
+Expr Env::get(const std::string& key) const{
+    if(this->m_bindings.count(key)){
+        return this->m_bindings.at(key);
+    }
+    if(this->parent){
+        return this->parent->get(key);
+    }
+    throw std::runtime_error("Undefined variable: " + key);
+}
+
 /*
 class Env final {
 public:
 
 
-
-Expr Env::get(const std::string& key) const{}
 bool Env::hashKey(const std::string& key) const{}
     Env* parent;
 
