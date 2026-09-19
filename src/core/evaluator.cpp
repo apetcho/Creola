d4a338df71a2084e94182966693fd8aa64ace0f5
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-19 19:01:35 +0100

    Implement creola::Mathfn() utility class

--- src/core/evaluator.cpp
+++ src/core/evaluator.cpp
@@ -1,3 +1,8 @@
+#include "ast.hpp"
+#include "evaluator.hpp"
+
+#include<stdexcept>
+#include<cmath>
 
 
 // -*----------------------------------------------------------------*-
@@ -6,6 +11,52 @@
 namespace creola{
 // -
 
+struct MathFn{
+    // -*-
+    double operator()(const std::string& name, double x){
+        if(name=="sin"){ return std::sin(x); }
+        if(name=="cos"){ return std::cos(x); }
+        if(name=="tan"){ return std::tan(x); }
+        if(name=="asin"){ return std::asin(x); }
+        if(name=="acos"){ return std::acos(x); }
+        if(name=="atan"){ return std::atan(x); }
+        if(name=="sinh"){ return std::sinh(x); }
+        if(name=="cosh"){ return std::cosh(x); }
+        if(name=="tanh"){ return std::tanh(x); }
+        if(name=="asinh"){ return std::asinh(x); }
+        if(name=="acosh"){ return std::acosh(x); }
+        if(name=="atanh"){ return std::atanh(x); }
+        if(name=="exp"){ return std::exp(x); }
+        if(name=="sqrt"){
+            if(x < 0.0){
+                throw std::runtime_error("'sqrt' expects positive argument.");
+            }    
+            return std::sqrt(x);
+        }
+        if(name=="ln"){
+            if(x <= 0.0){
+                throw std::runtime_error("'ln' expects strictly positive argument.");
+            }
+            return std::log(x);
+        }
+        if(name=="log"){
+            if(x <= 0.0){
+                throw std::runtime_error("'log' expects strictly positive argument.");
+            }
+            return std::log10(x);
+        }
+        if(name=="abs"){ return std::abs(x); }
+        throw std::runtime_error("not implemented unary math function '" + name + "'.");
+    }
+
+    // -*-
+    double operator()(const std::string& name, double x, double y){
+        if(name=="pow"){ return std::pow(x, y); }
+        if(name=="min"){ return std::min(x, y); }
+        if(name=="max"){ return std::max(x, y); }
+        throw std::runtime_error("not implemented binary math function '" + name + "'.");
+    }
+};
 
 // -*----------------------------------------------------------------*-
 }//-*- end::namespace::creola                                       -*-
