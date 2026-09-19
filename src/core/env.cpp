55a2838dfa84319a7e032d653a14b5b503d6f93f
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-19 14:55:48 +0100

    Implement creola::Env::Env(...) constructor methods

--- src/core/env.cpp
+++ src/core/env.cpp
@@ -1,4 +1,4 @@
-
+#include "env.hpp"
 
 // -*----------------------------------------------------------------*-
 // -*- begin::namespace::creola                                     -*-
@@ -6,6 +6,29 @@
 namespace creola{
 // -
 
+Env::Env()
+: parent{nullptr}
+{}
+
+
+Env::Env(Env* parent)
+: parent{parent}
+{}
+
+/*
+class Env final {
+public:
+
+
+void Env::put(const std::string& key, Expr expr){}
+Expr Env::get(const std::string& key) const{}
+bool Env::hashKey(const std::string& key) const{}
+    Env* parent;
+
+private:
+    std::map<std::string, Expr> m_bindings;
+};
+*/
 
 // -*----------------------------------------------------------------*-
 }//-*- end::namespace::creola                                       -*-
