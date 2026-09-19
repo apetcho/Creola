7fe7fd7edab094dd63de604299155de09b77af33
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-19 18:13:57 +0100

    Implement creola::is_number() utility function

--- src/core/evaluator.hpp
+++ src/core/evaluator.hpp
@@ -54,10 +54,15 @@ private:
 
 };
 
+// -*-
 static inline bool is_symbol(Expr expr){
     return std::dynamic_pointer_cast<Symbol>(expr)!=nullptr;
 }
 
+// -*-
+static inline bool is_number(Expr expr){
+    return std::dynamic_pointer_cast<Number>(expr)!=nullptr;
+}
 
 // -*----------------------------------------------------------------*-
 }//-*- end::namespace::creola                                       -*-
